91e4a727339754c713b464dcb3cfa8235f90b029
Laine <lolyawn@gmail.com>
2017-02-04 13:42:27 -0500

    begun progress on results page

---
+++ app/src/main/java/com/qhacks/moledetector/ResultsContainer.java
@@ -0,0 +1,13 @@
+package com.qhacks.moledetector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultsContainer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results_container);
+    }
+}
