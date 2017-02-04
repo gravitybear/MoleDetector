e82c2437e7bd4f4af5d5fc821e6eaaa87b1b3144
Laine <lolyawn@gmail.com>
2017-02-04 09:08:51 -0500

    refactored camera activities

--- app/src/main/java/com/qhacks/moledetector/MainActivity.java
+++ app/src/main/java/com/qhacks/moledetector/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                setContentView(R.layout.activity_camera_function);
             }
         });
 
