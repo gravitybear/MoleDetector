83050797aa980881c2e9a5d5fd39c5ff12a6e6c4
Laine <lolyawn@gmail.com>
2017-02-04 02:57:23 -0500

    adding progress on main page

--- app/src/main/java/com/qhacks/moledetector/MainActivity.java
+++ app/src/main/java/com/qhacks/moledetector/MainActivity.java
@@ -1,7 +1,12 @@
 package com.qhacks.moledetector;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        TextView title = (TextView)findViewById(R.id.appName);
+        TextView fact = (TextView)findViewById(R.id.scrollingFacts);
+        Typeface font_ubuntu_bold = Typeface.createFromAsset(getAssets(),  "fonts/Ubuntu-Bold.ttf");
+        Typeface font_ubuntu = Typeface.createFromAsset(getAssets(),  "fonts/Ubuntu-Regular.ttf");
+
+        title.setTypeface(font_ubuntu_bold);
+        fact.setTypeface(font_ubuntu);
     }
 }
