61b8fe82d658bfe687eded0950ef7c895f81c03b
Laine <lolyawn@gmail.com>
2017-02-04 10:57:09 -0500

    changed main page

--- app/src/main/java/com/qhacks/moledetector/MainActivity.java
+++ app/src/main/java/com/qhacks/moledetector/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -21,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        CoordinatorLayout mainPage = (CoordinatorLayout) findViewById(R.id.activity_main);
+        mainPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
