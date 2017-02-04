f0668f7029464cd21a1939369e04ef0a258ab99d
Laine <lolyawn@gmail.com>
2017-02-04 03:30:24 -0500

    latest changes to main screen

---
+++ app/src/main/java/com/qhacks/moledetector/camera_function.java
@@ -0,0 +1,13 @@
+package com.qhacks.moledetector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class camera_function extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_function);
+    }
+}
