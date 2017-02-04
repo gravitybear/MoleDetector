b5a0b5863d3c6e369eadf3dfd2b04bee2269cca1
matheesanManokaran <matheesan.manokaran@ryerson.ca>
2017-02-04 03:41:47 -0500

    camera feils from nutri
    added camera files to be worked on

---
+++ app/src/main/java/com/qhacks/moledetector/CameraPreview.java
@@ -0,0 +1,88 @@
+package io.alstonlin.hackprinceton;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * A Custom View that displays exactly what the given Camera Object sees; use this in
+ * the CameraFragment.
+ */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+    private Context context;
+
+    public CameraPreview(Context context) {
+        super(context);
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        // deprecated setting, but required on Android versions prior to 3.0
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        this.context = context;
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+        try {
+            mCamera = CameraFragment.getCameraInstance();
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Toast.makeText(context, "Error creating Camera Preview", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (mCamera != null) {
+            mCamera.setPreviewCallback(null);
+            mHolder.removeCallback(this);
+            mCamera.release();
+            CameraFragment.resetCamera();
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        if (mHolder.getSurface() == null){
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e){
+            // ignore: tried to stop a non-existent preview
+        }
+
+
+        Camera.Parameters params = mCamera.getParameters();
+        List<Camera.Size> supportedSizes = params.getSupportedPictureSizes();
+
+        Camera.Size sizePicture = supportedSizes.get(supportedSizes.size()/2);
+        params.setPictureSize(sizePicture.width, sizePicture.height);
+        mCamera.setParameters( params );
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+        // start preview with new settings
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+
+        } catch (Exception e){
+            Toast.makeText(context, "Error starting Camera Preview", Toast.LENGTH_LONG).show();;
+        }
+    }
+}
